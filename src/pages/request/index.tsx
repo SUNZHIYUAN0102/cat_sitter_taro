76ebff228402d765c0fef0df75b4806357a68def
unknown <zhiyuansun703@gmail.com>
2024-06-11 15:41:09 +0200

    complete all pages

--- src/pages/request/index.tsx
+++ src/pages/request/index.tsx
@@ -1,15 +1,12 @@
 import { View } from "@tarojs/components";
-import Content from "../index/components/content";
 import Footer from "../index/components/footer";
 import NavBar from "../index/components/navbar";
-
 
 export default function RequestPage() {
     return (
         <View className="page">
             <NavBar></NavBar>
             <View className="px-[60px]">
-                123
             </View>
             <View className="w-[100%] absolute bottom-0">
                 <Footer></Footer>
